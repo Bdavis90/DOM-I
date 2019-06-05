24ef6066b363f6a4cab4ab14ce2e1d537be34b38
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-06-05 00:52:01 -0400

    MVP

--- js/index.js
+++ js/index.js
@@ -40,3 +40,42 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navItems = document.querySelectorAll('a');
+navItems[0].textContent = "Services"
+navItems[1].textContent = "Product"
+navItems[2].textContent = "Vision"
+navItems[3].textContent = "Features"
+navItems[4].textContent = "About"
+navItems[5].textContent = "Contact"
+
+const codeLogo = document.querySelector("#cta-img");
+codeLogo.src = 'img/header-img.png'
+
+const ctaHeader = document.querySelector('h1');
+ctaHeader.textContent = "DOM Is Awesome"
+
+const ctaButton = document.querySelector('button');
+ctaButton.textContent = 'Get Started';
+
+const contentHeader = document.querySelectorAll('h4');
+contentHeader[0].textContent = 'Features'
+contentHeader[1].textContent = 'About'
+contentHeader[2].textContent = 'Services'
+contentHeader[3].textContent = 'Product'
+contentHeader[4].textContent = 'Vision'
+contentHeader[5].textContent = 'Contact'
+
+const contentInfo = document.querySelectorAll('p');
+contentInfo[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+contentInfo[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+contentInfo[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+contentInfo[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+contentInfo[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+contentInfo[5].textContent = '123 Way 456 Street Somewhere, USA'
+contentInfo[6].textContent = '1 (888) 888-8888'
+contentInfo[7].textContent = 'sales@greatidea.io'
+contentInfo[8].textContent = 'Copyright Great Idea! 2018'
+
+const middleImg = document.querySelector('#middle-img');
+middleImg.src = 'img/mid-page-accent.jpg'
